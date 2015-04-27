60345ab0196146a486f4e9b9a899c1fc1796daeb
Ryan Williams <row2000@gmail.com>
2015-04-27 09:25:42 +0200

    Initial Commit

---
+++ BlocBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocBrowser
+//
+//  Created by Ryan Williams on 27/04/15.
+//  Copyright (c) 2015 Ryan Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BlocBrowser/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlocBrowser
+//
+//  Created by Ryan Williams on 27/04/15.
+//  Copyright (c) 2015 Ryan Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
